1475a17b7862d2438014a779dbf96b8b5218278e
neal-o-r <neal@n-o-r.xyz>
2017-04-29 00:47:17 +0100

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# Serpinksi
+---
+
+An example of an iterated function system that generates the Serpinski Triangle.
+
+![Serpinski](https://github.com/neal-o-r/serpinski/blob/master/serpinski.png)
+
+
---
+++ serpinsk.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+
+a = [0, 0]
+b = [1, 0]
+c = [0.5, 1]
+
+p = np.random.rand(2)
+
+x, y = np.zeros(5000), np.zeros(5000)
+for i in range(5000):
+
+	r = np.random.choice([1 ,2, 3])
+	if r == 1:
+		s = a
+	if r == 2:
+		s = b
+	if r == 3:
+		s = c
+
+	p1 = [(p[0] + s[0]) / 2, (p[1] + s[1]) / 2]
+		
+	x[i] = p1[0]
+	y[i] = p1[1]
+	p = p1
+
+
+plt.scatter([a[0]], [a[1]], c='k')
+plt.scatter([b[0]], [b[1]], c='k')
+plt.scatter([c[0]], [c[1]], c='k')
+
+plt.scatter(x, y, s=1, alpha=0.7, marker='.', c='0.75')
+plt.axis('off')
+plt.show()
+
